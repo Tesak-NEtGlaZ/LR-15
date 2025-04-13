b4fdaaca3aa1721f3d29eced7f4df5dfb0b61c9d
Tesak-NEtGlaZ <lvm07kirov@gmail.com>
2025-04-14 00:01:08 +0300

    Add files via upload

---
+++ z1.pas
@@ -0,0 +1,22 @@
+﻿program StudentRecord;
+
+type
+  anketa = record
+    fio: string;
+    birth: string;
+    kurs: 1..5;
+  end;
+
+var
+  student: anketa;
+
+begin
+  student.fio := 'Иванов Иван Иванович';
+  student.birth := '01.01.2000';
+  student.kurs := 3;
+
+  writeln('Анкетные данные студента:');
+  writeln('Ф.И.О.: ', student.fio);
+  writeln('Дата рождения: ', student.birth);
+  writeln('Курс: ', student.kurs);
+end.
