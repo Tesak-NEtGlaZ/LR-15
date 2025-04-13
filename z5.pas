35fb68b649b9356248d3f4a0a837b29e8487d379
Tesak-NEtGlaZ <lvm07kirov@gmail.com>
2025-04-14 00:20:46 +0300

    Add files via upload

---
+++ z5.pas
@@ -0,0 +1,22 @@
+﻿program YearEnding;
+
+var
+  number: integer;
+  ending: string;
+
+begin
+  write('Введите количество лет: ');
+  readln(number);
+
+  if (number mod 100 >= 11) and (number mod 100 <= 14) then
+    ending := 'лет'
+  else
+    case number mod 10 of
+      1: ending := 'год';
+      2..4: ending := 'года';
+    else
+      ending := 'лет';
+    end;
+
+  writeln(number, ' ', ending);
+end.
